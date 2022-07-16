3bc8911d37b13d639b3af6b725b8ce26e542e03d
Uchqun Azimboyev <79976346+MrUchqun@users.noreply.github.com>
2022-07-16 15:54:37 +0500

    Initial commit

---
+++ app/src/main/java/com/example/android_graphql/activity/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.android_graphql.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.apollographql.apollo3.exception.ApolloException
+import com.example.android_graphql.*
+import com.example.android_graphql.network.GraphQL
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    private fun initViews() {
+        getUserList()
+    }
+
+    private fun getUserList() {
+        lifecycleScope.launch launchWhenResumed@{
+            val response = try {
+                GraphQL.get().query(UsersListQuery(20)).execute()
+            } catch (e: ApolloException) {
+                Log.d("@@@", "getUserList: $e")
+                return@launchWhenResumed
+            }
+
+            val users = response.data?.users
+            Log.d("@@@", "getUserList: ${users!!.size}")
+        }
+    }
+
+    private fun insertUser(name: String, rocket: String, twitter: String) {
+        lifecycleScope.launch launchWhenResumed@{
+            val result = try {
+                GraphQL.get().mutation(
+                    InsertUserMutation(name, rocket, twitter)
+                ).execute()
+            } catch (e: ApolloException) {
+                Log.d("MainActivity", e.toString())
+                return@launchWhenResumed
+            }
+            Log.d("MainActivity", result.toString())
+        }
+    }
+
+    private fun updateUser(id: String, name: String, rocket: String, twitter: String) {
+        lifecycleScope.launch launchWhenResumed@{
+            val result = try {
+                GraphQL.get().mutation(
+                    UpdateUserMutation(id, name, rocket, twitter)
+                ).execute()
+            } catch (e: ApolloException) {
+                Log.d("MainActivity", e.toString())
+                return@launchWhenResumed
+            }
+            Log.d("MainActivity", result.toString())
+        }
+    }
+
+    private fun deleteUser(id: String) {
+        lifecycleScope.launch launchWhenResumed@{
+            val result = try {
+                GraphQL.get().mutation(DeleteUserMutation(id)).execute()
+            } catch (e: ApolloException) {
+                Log.d("MainActivity", e.toString())
+                return@launchWhenResumed
+            }
+            Log.d("MainActivity", result.toString())
+        }
+    }
+}
+
